a2730f839cc05d0447649ca2b0faaf513dfb2091
Chuanyang Zheng <17251311@life.hkbu.edu.hk>
2020-03-15 17:55:35 +0800

    Upload Needed Package

---
+++ src/AppKickstarter/misc/Msg.java
@@ -0,0 +1,56 @@
+package AppKickstarter.misc;
+
+
+//======================================================================
+// Msg
+public class Msg {
+    private String sender;
+    private MBox senderMBox;
+    private Type type;
+    private String details;
+
+    //------------------------------------------------------------
+    // Msg
+    public Msg(String sender, MBox senderMBox, Type type, String details) {
+	this.sender = sender;
+	this.senderMBox = senderMBox;
+	this.type = type;
+	this.details = details;
+    } // Msg
+
+
+    //------------------------------------------------------------
+    // getters
+    public String getSender()     { return sender; }
+    public MBox   getSenderMBox() { return senderMBox; }
+    public Type   getType()       { return type; }
+    public String getDetails()    { return details; }
+
+
+    //------------------------------------------------------------
+    // toString
+    public String toString() {
+	return sender + " (" + type + ") -- " + details;
+    } // toString
+
+
+    //------------------------------------------------------------
+    // Msg Types
+    public enum Type {
+	Terminate,		// Terminate the running thread
+	SetTimer,		// Set a timer
+	CancelTimer,		// Set a timer
+	Tick,			// Timer clock ticks
+	TimesUp,		// Time's up for the timer
+	Poll,			// Health poll
+	PollAck,		// Health poll acknowledgement
+	Hello,			// Hello
+
+    Gate,          //From Gate
+    Dispatcher,		// Terminate the running thread
+    PayMachine,		// Set a timer
+    Collector,		// Set a timer
+    MotionSensor,			// Timer clock ticks
+
+    } // Type
+} // Msg
